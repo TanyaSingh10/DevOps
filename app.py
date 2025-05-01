dc7692e0f5df6ae44905ed02f3deb237e0c10215
TanyaSingh10 <tanyasingh10052004@gmail.com>
2025-05-02 01:04:00 +0530

    A little change in app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask!"
+    print(f"Request from: {request.remote_addr}")
+    return "Hello from Flask integrated with AWS!"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
 
